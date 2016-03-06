2ccc5eff1ce4cd3df41da5465f6060c2c3a5cb4e
frolk <frolkin-stas@yandex.ru>
2016-03-06 12:13:01 +0500

    continueRefactoring

--- main.c
+++ main.c
@@ -19,41 +19,6 @@ char *StrScaleDetectptr;
 char *StrPWMValueptr;
 char *StrOCRptr;
 
-char* shift_and_mul_utoa16(uint16_t n, char *buffer)
-{
-	uint8_t d4, d3, d2, d1, q, d0;
-
-	d1 = (n>>4)  & 0xF;
-	d2 = (n>>8)  & 0xF;
-	d3 = (n>>12) & 0xF;
-
-	d0 = 6*(d3 + d2 + d1) + (n & 0xF);
-	q = (d0 * 0xCD) >> 11;
-	d0 = d0 - 10*q;
-
-	d1 = q + 9*d3 + 5*d2 + d1;
-	q = (d1 * 0xCD) >> 11;
-	d1 = d1 - 10*q;
-
-	d2 = q + 2*d2;
-	q = (d2 * 0x1A) >> 8;
-	d2 = d2 - 10*q;
-
-	d3 = q + 4*d3;
-	d4 = (d3 * 0x1A) >> 8;
-	d3 = d3 - 10*d4;
-
-	char *ptr = buffer;
-	*ptr++ = ( d4 + '0' );
-	*ptr++ = ( d3 + '0' );
-	*ptr++ = ( d2 + '0' );
-	*ptr++ = ( d1 + '0' );
-	*ptr++ = ( d0 + '0' );
-	*ptr = 0;
-
-	while(buffer[0] == '0') ++buffer;
-	return buffer;
-}
 
 int main(void)
 {
